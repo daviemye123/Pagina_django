c2c102aa6c9a0b292ffed4ab32aa31c70253e969
david pedraza <davlpedraza@soy.sena.edu.co>
2025-11-13 07:50:14 -0500

    quitar descripcion

--- my_tennis_club/my_tennis_club/settings.py
+++ my_tennis_club/my_tennis_club/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-56h$7o9d*qw0fdleo(h*tbmgf8!id$!p73%qi5x@&!#3s2_2j3
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
