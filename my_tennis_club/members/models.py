5f6554bf8112e7805a6a2baa5907135fce7da78f
david pedraza <davlpedraza@soy.sena.edu.co>
2025-11-18 07:31:25 -0500

    base de datos

--- my_tennis_club/members/models.py
+++ my_tennis_club/members/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Member(models.Model):
+  firstname = models.CharField(max_length=255)
+  lastname = models.CharField(max_length=255)
+  phone = models.IntegerField(null=True)
+  joined_date = models.DateField(null=True)
+  video_game_favorite = models.CharField(max_length=100, null=True, blank=True)
+  amd_intel = models.CharField(max_length=10, null=True, blank=True)
+  music_favorite=models.BooleanField(null=True)  
+  
