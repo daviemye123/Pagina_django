615d797abd944e13d8dc28c5bfa4a44b344b693a
david pedraza <davlpedraza@soy.sena.edu.co>
2025-11-25 06:50:32 -0500

    queny set

--- my_tennis_club/members/models.py
+++ my_tennis_club/members/models.py
@@ -5,7 +5,7 @@ class Member(models.Model):
   lastname = models.CharField(max_length=255)
   phone = models.IntegerField(null=True)
   joined_date = models.DateField(null=True)
-  video_game_favorite = models.CharField(max_length=100, null=True)
+  video_game_favorite = models.CharField(max_length=100)
   amd_intel = models.CharField(max_length=255, null=True)
   music_favorite=models.BooleanField(max_length=255)
   def __str__(self):
--- my_tennis_club/members/views.py
+++ my_tennis_club/members/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse
 from django.template import loader
 from .models import Member
+from django.db.models import Q
 
 def members(request):
   mymembers = Member.objects.all().values()
@@ -21,3 +22,18 @@ def details(request, id):
 def main(request):
   template = loader.get_template('main.html')
   return HttpResponse(template.render())
+def testing(request):
+  template = loader.get_template('template.html')
+  members = Member.objects.all().values()
+  columna = Member.objects.all().values("video_game_favorite") 
+  mydata = Member.objects.filter(Q(firstname='ranni') | Q(firstname='tilin')).values()
+  mydata = Member.objects.filter(lastname__icontains='caria').values()
+  mydata = Member.objects.filter(id__gte=3).values()
+  mydata = Member.objects.all().order_by('firstname').values()
+  context = {
+    'fruits': ['Apple', 'Banana', 'Cherry'],
+    'members': members,
+    'columna': columna,
+    'mydata': mydata,  # Agregadas las variables al contexto
+  }
+  return HttpResponse(template.render(context, request))
